32c5a7a0f71d01f8288a247363f1bca227303a5a
Santiagodxz <santiagodiazdw@gmail.com>
2023-10-04 15:26:23 -0500

    Tarea 1 punto 1 terminado

---
+++ js/task-01.js
@@ -0,0 +1,4 @@
+var categoryList = document.querySelector("#categories");
+var categoryItem = categoryList.querySelectorAll("li.item");
+var numberOfCategories = categoryItem.length;
+console.log("Number of categories:", numberOfCategories);
