78aa73b21c6664f11e560d77746dcf3ab65d292f
Santiagodxz <santiagodiazdw@gmail.com>
2023-10-05 11:41:25 -0500

    Tarea 1 punto 8 terminado

--- js/task-08.js
+++ js/task-08.js
@@ -4,3 +4,19 @@ loginForm.addEventListener("submit", function (event) {
   event.preventDefault();
 
   const emailInput = this.elements.email;
+  const passwordInput = this.elements.password;
+
+  if (!emailInput.value.trim() || !passwordInput.value.trim()) {
+    alert("Todos los campos deben ser completados");
+    return;
+  }
+
+  const formData = {
+    email: emailInput.value,
+    password: passwordInput.value,
+  };
+
+  console.log(formData);
+
+  this.reset();
+});
