277219c01c974cd5a63f9f3f80cc5a5b11ac4847
Santiagodxz <santiagodiazdw@gmail.com>
2023-10-04 19:41:34 -0500

    Tarea 1 punto 2 terminado

--- js/task-01.js
+++ js/task-01.js
@@ -2,3 +2,15 @@ var categoryList = document.querySelector("#categories");
 var categoryItem = categoryList.querySelectorAll("li.item");
 var numberOfCategories = categoryItem.length;
 console.log("Number of categories:", numberOfCategories);
+
+categoryItem.forEach((categoryItem) => {
+  var categoryTitle = categoryItem.querySelector("h2").textContent;
+
+  var categoryElements = categoryItem.querySelectorAll("ul li");
+
+  var numberOfElements = categoryElements.length;
+
+  console.log("Category:", categoryTitle);
+  
+  console.log("Elements:", numberOfElements);
+});
--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,33 @@
-const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+// const ingredients = [
+//   'Potatoes',
+//   'Mushrooms',
+//   'Garlic',
+//   'Tomatos',
+//   'Herbs',
+//   'Condiments',
+// ];
+
+var ingredientsLi = document.querySelector("ingredients");
+
+var ingredients =[
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatoes",
+  "Herbs",
+  "Condiments",
 ];
+
+  const ingredientsLiItem = [];
+  
+  for (let i = 0; i < ingredients.length; i++) {
+    const newItem = document.createElement("li");
+    newItem.innerText= ingredients[i];
+    newItem.classList.add("item");
+    ingredientsLiItem.push(newItem);
+  }
+
+  ingredientsLiItem.forEach((li) => {
+    document.querySelector("ul").append(li);
+  });
+  
